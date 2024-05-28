ad22c42fa00ef74ff2b7447bd75dcfddc91a2a99
Coolinar <lovordev@gmail.com>
2024-05-28 23:01:06 +0500

    Add item config editor

---
+++ Assets/Clicker/Scripts/Editor/ItemConfigDrawer.cs
@@ -0,0 +1,32 @@
+﻿using Clicker.Scripts.Runtime.Config;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ItemConfig))]
+public class ItemConfigDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUILayout.PropertyField(property.FindPropertyRelative("<ItemType>k__BackingField"), label);
+
+        var valueCostPairs = property.FindPropertyRelative("<ValueCostPairs>k__BackingField");
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Value Cost Pairs");
+        valueCostPairs.arraySize = EditorGUILayout.IntField(valueCostPairs.arraySize, GUILayout.Width(50));
+        EditorGUILayout.EndHorizontal();
+        
+        EditorGUIUtility.labelWidth = 50;
+        for (var i = 0; i < valueCostPairs.arraySize; i++)
+        {
+            var valueCostPair = valueCostPairs.GetArrayElementAtIndex(i);
+            EditorGUILayout.BeginHorizontal();
+
+            EditorGUILayout.LabelField($"{i + 1}) ", GUILayout.Width(20));
+            EditorGUILayout.PropertyField(valueCostPair.FindPropertyRelative("<Value>k__BackingField"), GUIContent.none);
+            EditorGUILayout.PropertyField(valueCostPair.FindPropertyRelative("<Cost>k__BackingField"), GUIContent.none);
+
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+}
